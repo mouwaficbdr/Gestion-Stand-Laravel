e4da7df9716a5cfb7378793485585a535c7f5580
Florent BOUDZOUMOU <109081275+Florent242@users.noreply.github.com>
2025-07-21 22:13:03 +0200

    Modification bootstrap pour la gestion des roles grace aux middlewares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,13 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\AdminMiddleware::class,
+            'approved.entrepreneur' => \App\Http\Middleware\ApprovedEntrepreneurMiddleware::class,
+            'pending' => \App\Http\Middleware\PendingMiddleware::class,
+        ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
